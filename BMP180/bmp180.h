5e3bea8c43469149216ae33a382b6b8f031c4d0c
koksoybedirhan <71734713+koksoybedirhan@users.noreply.github.com>
2022-09-14 00:34:46 +0300

    Update bmp180.h

--- BMP180/bmp180.h
+++ BMP180/bmp180.h
@@ -1,16 +1,23 @@
 /*
  * bmp180.h
  *
- *  Created on: Sep 12, 2022
+ *  Created on: Sep 13, 2022
  *      Author: b1d0
  */
 
 #ifndef INC_BMP180_H_
 #define INC_BMP180_H_
 
-#include <stdio.h>
 #include "stm32f4xx_hal.h"
 
-int BMP180_Test(void);
+void BMP180_Test(void); //This function for testing device working
+
+void BMP180_Start (void); //This function writing bmp180 register data in memory
+
+float BMP180_Calc_Temp (void); //This function give the calculating temperature
+
+float BMP180_Calc_Press(int oss); //This function give the calculating pressure
+
+float BMP180_Calc_Alt (int oss); //This function give the calculating altitude
 
 #endif /* INC_BMP180_H_ */

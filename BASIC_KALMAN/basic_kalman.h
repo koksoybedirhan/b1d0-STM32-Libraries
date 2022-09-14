c8b7f652c71419a2cedfde245a9f9e0385bf4348
koksoybedirhan <71734713+koksoybedirhan@users.noreply.github.com>
2022-09-14 15:12:45 +0300

    Update basic_kalman.h

--- BASIC_KALMAN/basic_kalman.h
+++ BASIC_KALMAN/basic_kalman.h
@@ -8,7 +8,7 @@
 #ifndef INC_BASIC_KALMAN_H_
 #define INC_BASIC_KALMAN_H_
 
-#include "stm32f4xx_hal.h" //optional library, but it's recomenned to add for STM32 drivers.
+#include "stm32f4xx_hal.h" //Optional library, but it's recomenned to add for STM32 drivers.
 
 double Kalman (double U);
 
